31d23b7aeaabcd91e0827d7f920023344693ddeb
Didi518 <94155950+Didi518@users.noreply.github.com>
2023-02-09 20:32:42 +0100

    ✨ Adding create & update product

--- src/Controller/Admin/ProductController.php
+++ src/Controller/Admin/ProductController.php
@@ -3,9 +3,13 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Product;
+use App\Form\ProductFormType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\String\Slugger\SluggerInterface;
 
 #[Route('/admin/articles', name: 'admin_products_')]
 class ProductController extends AbstractController
@@ -17,17 +21,53 @@ class ProductController extends AbstractController
     }
     
     #[Route('/ajout', name: 'add')]
-    public function add(): Response
+    public function add(Request $request, EntityManagerInterface $manager, SluggerInterface $slugger): Response
     {
         $this->denyAccessUnlessGranted('ROLE_ADMIN');
-        return $this->render('admin/products/index.html.twig');
+        
+        $product = new Product();
+
+        $productForm = $this->createForm(ProductFormType::class, $product);
+        $productForm->handleRequest($request);
+        
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            $slug = $slugger->slug($product->getName());
+            $product->setSlug($slug);
+
+            $manager->persist($product);
+            $manager->flush();
+
+            $this->addFlash('success', 'Article ajouté avec succès');
+            return $this->redirectToRoute('admin_products_index');
+        }
+
+        return $this->render('admin/products/add.html.twig', [
+            'productForm' => $productForm->createView()
+        ]);
     }
     
     #[Route('/edition/{id}', name: 'edit')]
-    public function edit(Product $product): Response
+    public function edit(Product $product, Request $request, EntityManagerInterface $manager, SluggerInterface $slugger): Response
     {
-        $this->denyAccessUnlessGranted('PRODUCT_EDIT', $product);   
-        return $this->render('admin/products/index.html.twig');
+        $this->denyAccessUnlessGranted('PRODUCT_EDIT', $product); 
+
+        $productForm = $this->createForm(ProductFormType::class, $product);
+        $productForm->handleRequest($request);
+        
+        if ($productForm->isSubmitted() && $productForm->isValid()) {
+            $slug = $slugger->slug($product->getName());
+            $product->setSlug($slug);
+
+            $manager->persist($product);
+            $manager->flush();
+
+            $this->addFlash('success', 'Article modifié avec succès');
+            return $this->redirectToRoute('admin_products_index');
+        }
+
+        return $this->render('admin/products/edit.html.twig', [
+            'productForm' => $productForm->createView()
+        ]);
     }
     
     #[Route('/suppression/{id}', name: 'delete')]
---
+++ src/Form/ProductFormType.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Category;
+use App\Entity\Product;
+use App\Repository\CategoryRepository;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', options: [
+                'label' => 'Nom',
+            ])
+            ->add('description')
+            ->add('price', options: [
+                'label' => 'Prix',
+            ])
+            ->add('stock', options: [
+                'label' => 'Unités en stock',
+            ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'label' => 'Catégorie',
+                'group_by' => 'parent.name',
+                'query_builder' => function (CategoryRepository $cr) {
+                    return $cr->createQueryBuilder('c')
+                        ->where('c.parent IS NOT NULL')
+                        ->orderBy('c.name', 'ASC')
+                    ;
+                }
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
